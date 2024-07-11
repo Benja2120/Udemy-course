2989c88cb998a5f0c13dda9bbf39da932f68cbf6
Benja2120 <144168726+Benja2120@users.noreply.github.com>
2024-07-11 17:20:00 -0500

    Audio in

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,16 @@
+var numOfDrums = document.querySelectorAll(".drum").length;
+for(i=0;i<numOfDrums;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var buttonInnerHTML = this.innerHTML;
+
+    switch(buttonInnerHTML){
+      case("w"):
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+
+        break;
+      default:
+    }
+
+  });
+}
